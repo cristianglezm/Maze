ba0ec642dc930b22eff4152592dc82c055047bbf
Cristian González <cristian.glez.m@gmail.com>
2019-03-22 01:51:50 +0100

    changed configuration for android

--- android/app/src/main/jni/Application.mk
+++ android/app/src/main/jni/Application.mk
@@ -1,5 +1,5 @@
-NDK_TOOLCHAIN_VERSION := 7.0
+NDK_TOOLCHAIN_VERSION := 7.0.2
 APP_PLATFORM := android-14
-APP_STL := c++_static
+APP_STL := c++_shared
 APP_ABI := armeabi-v7a
 APP_MODULES := sfml-activity Maze
