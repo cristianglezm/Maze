67cd8815ec8c6fa66bb5b1e1ecff4c8b93a09e31
Cristian Gonzalez <cristian.glez.m@gmail.com>
2023-01-31 18:59:35 +0100

    Removed travis, added github actions
    Added debug / release version for android
    changed sfml ver to 2.5

--- android/app/src/main/jni/Android.mk
+++ android/app/src/main/jni/Android.mk
@@ -9,18 +9,29 @@ LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../../../../src
 LOCAL_SRC_FILES := ../../../../../src/androidMain.cpp ../../../../../src/Graph.cpp
 LOCAL_SRC_FILES += ../../../../../src/Tile.cpp ../../../../../src/Menu.cpp ../../../../../src/Button.cpp
 
-LOCAL_LDLIBS := -llog
-
-LOCAL_SHARED_LIBRARIES := sfml-system
-LOCAL_SHARED_LIBRARIES += sfml-window
-LOCAL_SHARED_LIBRARIES += sfml-graphics
-LOCAL_SHARED_LIBRARIES += sfml-audio
-LOCAL_SHARED_LIBRARIES += sfml-network
-LOCAL_SHARED_LIBRARIES += openal
-LOCAL_WHOLE_STATIC_LIBRARIES := sfml-main
-
+ifeq ($(BUILD_TYPE), debug)
+    LOCAL_LDLIBS := -llog
+    LOCAL_SHARED_LIBRARIES := sfml-system-d
+    LOCAL_SHARED_LIBRARIES += sfml-window-d
+    LOCAL_SHARED_LIBRARIES += sfml-graphics-d
+    LOCAL_SHARED_LIBRARIES += sfml-audio-d
+    LOCAL_SHARED_LIBRARIES += sfml-network-d
+    LOCAL_SHARED_LIBRARIES += sfml-activity-d
+    LOCAL_SHARED_LIBRARIES += openal
+    LOCAL_WHOLE_STATIC_LIBRARIES := sfml-main-d
+else
+    LOCAL_LDLIBS := -llog
+    LOCAL_SHARED_LIBRARIES := sfml-system
+    LOCAL_SHARED_LIBRARIES += sfml-window
+    LOCAL_SHARED_LIBRARIES += sfml-graphics
+    LOCAL_SHARED_LIBRARIES += sfml-audio
+    LOCAL_SHARED_LIBRARIES += sfml-network
+    LOCAL_SHARED_LIBRARIES += sfml-activity
+    LOCAL_SHARED_LIBRARIES += openal
+    LOCAL_WHOLE_STATIC_LIBRARIES := sfml-main
+endif
 LOCAL_CPP_FEATURES := rtti exceptions
-LOCAL_CFLAGS += -Wall -std=c++11 -DANDROID
+LOCAL_CFLAGS += -Wall -std=c++17 -DANDROID
 
 include $(BUILD_SHARED_LIBRARY)
 
--- android/app/src/main/jni/Application.mk
+++ android/app/src/main/jni/Application.mk
@@ -1,5 +1,13 @@
-NDK_TOOLCHAIN_VERSION := 7.0.2
-APP_PLATFORM := android-9
+NDK_TOOLCHAIN_VERSION := 4.9
+APP_PLATFORM := android-14
 APP_STL := c++_shared
 APP_ABI := armeabi-v7a
-APP_MODULES := sfml-activity Maze
+APP_CPPFLAGS += -std=c++17
+ifeq ($(BUILD_TYPE), debug)
+    APP_OPTIM := debug
+    APP_MODULES := sfml-activity-d Maze
+else
+    APP_OPTIM := release
+    APP_MODULES := sfml-activity Maze
+endif
+APP_NAME := Maze
